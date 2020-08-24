7c7f20e2398d1a59ec97b0201ae1db635abf5955
yth <yth9105@gmail.com>
2020-08-24 19:50:53 +0900

    테스트 커밋

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
                 Log.d("test","test");
+                Log.d("name","Taeha");
             }
         });
     }
